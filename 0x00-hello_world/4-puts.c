f0ac938277133d32e33c6d348474690bcd90ba62
Mohammed <mm5151847@gmail.com>
2023-03-16 10:56:37 -0700

    ;

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio>
+/**
+ * main-entery point
+ *
+ *
+ *
+ * description-printing using puts function
+ *
+ * return always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+       	return (0);
+}
