2532821ac1bcaba664da217a2f8ded849c21f6eb
Mohammed Mahmoud <mm5151847@gmail.com>
2023-07-06 07:01:09 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,16 +3,13 @@
  *get_bit - return the value on a given index
  *@n: the integer number
  *@index: the given index
- *@f: integer to be shifted
  *return: the bit value
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 	{
-		return (-1);
+		return(-1)
 	}
-	int f = 1;
-	f = f << index;
-	return (n & f);
+	return (n << index & 1);
 }
