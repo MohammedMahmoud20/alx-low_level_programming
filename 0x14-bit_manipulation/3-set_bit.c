5f6277476c509716cd75d322a38e5337ffbcfe08
Mohammed Mahmoud <mm5151847@gmail.com>
2023-07-06 07:16:38 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,11 +6,11 @@
  *@nidex: the given index
  *return: 1 if it worked, -1 if there is an error
 */
-int set_bit(unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int * n, unsigned int index)
 {
-    if (index >= sizeof(n)*8 )
+    if ( index >= sizeof(n) * 8 )
     {
-        return (-1);
+	    return (-1);
     }
     return (!!(*n |= 1L << index));
 }
