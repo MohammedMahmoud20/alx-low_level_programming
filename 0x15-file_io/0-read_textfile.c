0858f2eb8b0b2580baabb7f6a3f074497fcfef8f
Mohammed Mahmoud <mm5151847@gmail.com>
2023-07-10 07:24:58 -0700

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,22 +5,26 @@
  *@filename: the name of the file which will be read
  *@letters: number of the letters in the file
  *@file_out_put: the varibalue contain the file
- *retun: 0 if there is an error 
+ *@buffer: to store the file character
+ *byte: to be printed in the stdout
+ *return: 0 if there is an error
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    FILE *file_out_put;
-    file_out_put = fopen ( filename,"r");
-    if(file_out_put!=NULL)
+    int file_out_put;
+    char buffer [1024*8];
+    file_out_put =open ( filename,"r");
+    if(!filename||!letters)
     {
-        char c;
-        while ((c=fgetc(file_out_put))!=EOF)
-        {
-            putchar(c);
-        }
-        fclose(file_out_put);
-        
-    }else{
         return 0;
     }
+    if(file_out_put=-1)
+    {
+        return 0;
+    }
+    ssize_t byte;
+    byte=read(file_out_put,&buffer[0],letters);
+    byte=write(STDOUT_FILENO,&buffer[0],byte);
+    close(file_out_put);
+    return (byte);
 }
