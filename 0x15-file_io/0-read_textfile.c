8fb9b5376fb9e048f9eb341ced801f3d10291c70
Mohammed Mahmoud <mm5151847@gmail.com>
2023-07-10 07:12:57 -0700

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,23 +4,23 @@
  *read_textfile - read a file and print it
  *@filename: the name of the file which will be read
  *@letters: number of the letters in the file
- *@counter: to count the number of the letters that has been read
  *@file_out_put: the varibalue contain the file
- *retun: void 
+ *retun: 0 if there is an error 
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     FILE *file_out_put;
-    size_t counter=0;
-    file_out_put = fopen("%s",filename, "r");
-    if(file_out_put==NULL)
+    file_out_put = fopen ( filename,"r");
+    if(file_out_put!=NULL)
     {
+        char c;
+        while ((c=fgetc(file_out_put))!=EOF)
+        {
+            putchar(c);
+        }
+        fclose(file_out_put);
+        
+    }else{
         return 0;
     }
-    while(fscanf(file_out_put,"%c",&file_out_put)!=EOF||counter!=letters)
-    {
-        counter++;
-        printf("%s\n",file_out_put);
-    }
-    fclose(file_out_put);
 }
